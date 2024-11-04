590d38293db7f8384382dfbd85f59232d4b7136f
Kássio San <kciosantana@gmail.com>
2024-11-04 20:09:38 -0300

    init front end

---
+++ front-end-patusco/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Button } from './ui/button';
+</script>
+
+<template>
+  <Button>Click me</Button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
