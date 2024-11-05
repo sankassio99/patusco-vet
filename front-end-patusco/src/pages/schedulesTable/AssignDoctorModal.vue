305faa94cd003a80feff78289dff8f6dde653aa8
Kássio San <kciosantana@gmail.com>
2024-11-05 07:12:32 -0300

    style

--- front-end-patusco/src/pages/schedulesTable/AssignDoctorModal.vue
+++ front-end-patusco/src/pages/schedulesTable/AssignDoctorModal.vue
@@ -22,23 +22,17 @@ import { Label } from '@components/ui/label'
     </DialogTrigger>
     <DialogContent class="sm:max-w-[425px]">
       <DialogHeader>
-        <DialogTitle>Edit profile</DialogTitle>
+        <DialogTitle>Assign doctor</DialogTitle>
         <DialogDescription>
-          Make changes to your profile here. Click save when you're done.
+            Assign a doctor to this schedule.
         </DialogDescription>
       </DialogHeader>
       <div class="grid gap-4 py-4">
         <div class="grid grid-cols-4 items-center gap-4">
           <Label for="name" class="text-right">
-            Name
+            Doctor
           </Label>
           <Input id="name" value="Pedro Duarte" class="col-span-3" />
-        </div>
-        <div class="grid grid-cols-4 items-center gap-4">
-          <Label for="username" class="text-right">
-            Username
-          </Label>
-          <Input id="username" value="@peduarte" class="col-span-3" />
         </div>
       </div>
       <DialogFooter>
