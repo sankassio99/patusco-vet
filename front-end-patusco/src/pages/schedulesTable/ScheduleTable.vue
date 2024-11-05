68c7ee0a058cd14170b4463012f78f4413de5705
Kássio San <kciosantana@gmail.com>
2024-11-04 21:46:34 -0300

    Create table component view

--- front-end-patusco/src/pages/schedulesTable/ScheduleTable.vue
+++ front-end-patusco/src/pages/schedulesTable/ScheduleTable.vue
@@ -1,8 +1,44 @@
 <script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@components/ui/table'
 </script>
 
 <template>
-  <div class="w-full">
-    aaaa
-  </div>
+  <Table>
+    <TableCaption>A list of your recent invoices.</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead class="w-[100px]">
+          Code
+        </TableHead>
+        <TableHead>Requester name</TableHead>
+        <TableHead>Animal name</TableHead>
+        <TableHead>Type</TableHead>
+        <TableHead>Date</TableHead>
+        <TableHead>Shift</TableHead>
+        <TableHead class="text-right">
+          Actions
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableCell class="font-medium">
+          INV001
+        </TableCell>
+        <TableCell>Paid</TableCell>
+        <TableCell>Credit Card</TableCell>
+        <TableCell class="text-right">
+          $250.00
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
 </template>
