2743a5d6d51df76524fad0bac81e3aa525a82c0e
Kássio San <kciosantana@gmail.com>
2024-11-05 20:57:02 -0300

    style

--- front-end-patusco/src/pages/home/Home.vue
+++ front-end-patusco/src/pages/home/Home.vue
@@ -9,38 +9,43 @@ function selectUser(userType: UserType) {
 </script>
 
 <template>
-    
-    <div class="flex flex-col justify-center w-full items-center space-y-5">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white w-2/4">
-            <div class="font-bold text-xl mb-2">Client</div>
-            <p class="text-gray-700 text-base">
-                This is a clien, you can make a appointment with him.
-            </p>
-            <button @click="selectUser(UserType.CLIENT)" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Select
-            </button>
-        </div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white w-2/4">
-            <div class="font-bold text-xl mb-2">Employee</div>
-            <p class="text-gray-700 text-base">
-                You can manage appointments and assign doctors.
-            </p>
-            <button @click="selectUser(UserType.EMPLOYEE)" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Select
-            </button>
-        </div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white w-2/4">
-            <div class="font-bold text-xl mb-2">Doctor</div>
-            <p class="text-gray-700 text-base">
-                You can manage appointments.
-            </p>
-            <button @click="selectUser(UserType.DOCTOR)" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Select
-            </button>
-        </div>
-    </div>
+    <div class="my-10">
+        <h1 class="text-4xl font-bold text-center mb-6">Login</h1>
 
+        <div class="flex flex-col justify-center w-full items-center space-y-5">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white w-2/4">
+                <div class="font-bold text-xl mb-2">Client</div>
+                <p class="text-gray-700 text-base">
+                    You can make a appointment with him.
+                </p>
+                <button @click="selectUser(UserType.CLIENT)"
+                    class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Sign in
+                </button>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white w-2/4">
+                <div class="font-bold text-xl mb-2">Employee</div>
+                <p class="text-gray-700 text-base">
+                    You can manage appointments and assign doctors.
+                </p>
+                <button @click="selectUser(UserType.EMPLOYEE)"
+                    class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Sign in
+                </button>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white w-2/4">
+                <div class="font-bold text-xl mb-2">Doctor</div>
+                <p class="text-gray-700 text-base">
+                    You can manage appointments.
+                </p>
+                <button @click="selectUser(UserType.DOCTOR)"
+                    class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Sign in
+                </button>
+            </div>
+        </div>
+
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
