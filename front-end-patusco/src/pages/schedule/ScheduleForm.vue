d8e2bcc5070a3658687c90ad39cf6198274f22ad
Kássio San <kciosantana@gmail.com>
2024-11-05 21:44:58 -0300

    load data in form

--- front-end-patusco/src/pages/schedule/ScheduleForm.vue
+++ front-end-patusco/src/pages/schedule/ScheduleForm.vue
@@ -2,49 +2,75 @@
 import { AutoForm } from '../../components/ui/auto-form'
 import { Button } from '../../components/ui/button'
 import * as z from 'zod'
-import ScheduleFormController from './ScheduleFormController'
+import ScheduleFormController from './ScheduleFormController';
+import { Schedule } from '../schedulesTable/models/scheduleModel';
+import {
+    CalendarDate,
+} from '@internationalized/date'
+
+const props = defineProps<{
+    schedule: Schedule;
+}>();
+
+enum AnimalType {
+    Dog = 'Dog',
+    Cat = 'Cat',
+    Bird = 'Bird',
+    Rabbit = 'Rabbit',
+    Fish = 'Fish',
+    Other = 'Other'
+}
+
+function getDate(): CalendarDate {
+    const date = props?.schedule?.date ? new Date(props?.schedule?.date) : new Date();
+
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+
+    return new CalendarDate(year, month, day)
+}
 
 const schema = z.object({
     yourName: z
         .string({
             required_error: 'User name is required.',
-        }),
+        })
+        .default(props?.schedule?.requesterName ?? ''),
 
     email: z
         .string({
             required_error: 'Email is required.',
-        }),
-    
+        })
+        .default(props?.schedule?.requesterEmail ?? ''),
+
     animalName: z
         .string({
             required_error: 'Animal Name is required.',
-        }),
+        })
+        .default(props?.schedule?.animalName ?? ''),
 
     age: z
         .number({
             required_error: 'Animal Age is required.',
-        }),
+        })
+        .default(props?.schedule?.animalAge ?? null),
 
     date: z.coerce.date({
         required_error: 'Date is required.',
-    }),
+    }).default(getDate()),
 
     animalType: z
-        .enum(['Dog', 'Cat', 'Bird', 'Rabbit', 'Fish', 'Other']),
+        .nativeEnum(AnimalType)
+        .default(props?.schedule?.type ?? AnimalType.Other),
 
     symptoms: z
-        .enum([
-            'Loss of appetite', 
-            'Fever', 
-            'Abnormal consistent of the faeces', 
-            'Vomiting',
-            'Diarrhea',
-            'Lethargy',
-            'Coughing',
-            'Sneezing',
-            'Itching',
-            'Others'
-        ]),
+        .array(
+        // Define the fields for each item
+            z.object({
+                name: z.string(),
+            })
+        ),
 
     shift: z
         .enum(['Morning', 'Afternoon']),
@@ -59,7 +85,6 @@ const controller = new ScheduleFormController();
         <h1 class="text-4xl font-bold text-center mb-6">Make an appointment</h1>
 
         <AutoForm class="w-2/3 space-y-6" :schema="schema" :field-config="{
-        
             animalType: {
                 label: 'Animal Type',
             },
@@ -67,7 +92,7 @@ const controller = new ScheduleFormController();
             shift: {
                 component: 'radio',
             },
-        
+
         }" @submit="controller.onSubmit">
             <Button type="submit">
                 Submit
--- front-end-patusco/src/pages/schedulesTable/models/scheduleModel.ts
+++ front-end-patusco/src/pages/schedulesTable/models/scheduleModel.ts
@@ -1,10 +1,13 @@
 import DoctorModel from "./doctorModel";
 
 export interface Schedule {
+    symptoms: string[];
+    animalAge: number;
     id: string;
     assigned: DoctorModel;
     code: string;
     requesterName: string;
+    requesterEmail: string;
     animalName: string;
     type: string;
     date: string;
