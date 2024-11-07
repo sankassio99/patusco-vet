178be7e35c63241ca01381cb20866c155b0f0dcd
Kássio San <kciosantana@gmail.com>
2024-11-06 21:50:07 -0300

    test_store_an_appointment (GREEN)

--- back-end/tests/Feature/ExampleTest.php
+++ back-end/tests/Feature/ExampleTest.php
@@ -9,11 +9,26 @@ class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
+     * 
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_store_an_appointment(): void
     {
-        $response = $this->get('/');
+        $response = $this->post('/appointment', [
+            'id' => 'some-id',
+            'clientName' => 'John Doe',
+            'symptoms' => 'Coughing',
+            'animalAge' => 5,
+            'assigned' => 'Dr. Smith',
+            'code' => 'APPT123',
+            'requesterName' => 'Jane Doe',
+            'requesterEmail' => 'jane.doe@example.com',
+            'animalName' => 'Buddy',
+            'type' => 'Dog',
+            'date' => '2023-10-01',
+            'shift' => 'Morning'
+        ]);
 
-        $response->assertStatus(200);
+        $response->assertStatus(201);
+        $response->assertJson(['message' => 'success!']);
     }
 }
